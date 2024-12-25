8c0d0cad8df07551efa9bdc4606d030730b105c0
Eaglorn <lexus1.45@mail.ru>
2024-12-25 13:02:20 +1000

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 private lateinit var server: Server
 
 fun main(args: Array<String>) {
-    if(args[0] == "server") runServer(args[1], args[2])
+    if (args.isNotEmpty()) {
+        if (args[0] == "server") runServer(args[1], args[2])
+    }
 }
 
 private fun runServer(pathImport: String, pathExport: String) {
